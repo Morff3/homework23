7aaf75708718e848f34da3e67477610c6c689bb5
Morff3 <166973084+Morff3@users.noreply.github.com>
2024-06-18 22:15:19 +0700

    Add files via upload

---
+++ homework23.py
@@ -0,0 +1,16 @@
+import time
+
+import os
+
+directory = 'C:/PythonProject/Homeworks'
+directory_normalized = os.path.normpath(directory)
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        file_size = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт, Время изменения: {formatted_time}, '
+              f'Родительская директория: {parent_dir}')
